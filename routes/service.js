4ae09823e0655975a1f70527ea423f8eb7ece39e
XeyberIgideli <d3facerx@gmail.com>
2023-09-07 19:52:48 +0400

    feat: created function to read show subtitles

--- routes/service.js
+++ routes/service.js
@@ -1,13 +1,70 @@
 import express from 'express'
 import axios from 'axios'
-import cheerio from 'cheerio';
-
+import cheerio from 'cheerio'
+import admZip from 'adm-zip'
+import {Readable} from 'stream'
 
 const router = express.Router()
 
-// Search by lang
-router.get('/search/:lang/:movieName', getMovie)
-router.get('/api/:movieName/:lang/:totLink/:num/readSubtitle', readMovieSubtitle)
+// Search movie subtitle
+router.get('/search/movie/:lang/:movieName', getMovie)
+// Read movie subtitle
+router.get('/search/:movieName/:lang/:totLink/:num/readSubtitle', readMovieSubtitle)
+// Search tv show subtitle
+router.get('/search/show/:lang/:showName', getShow)
+// Read show subtitle
+router.get('/search/:showName/:lang/:totLink/:num/readShowSubtitle', readShowSubtitle)
+
+async function readShowSubtitle(req,res) {
+  const paramLang =  req.params.lang //req.query.lang.split('.')[0]
+  const listData = req.query.listData
+  try {
+      if(paramLang) {
+          const totalLink = req.params.totLink
+          const showName = req.params.showName;
+          const showId = await getId(showName,paramLang);
+          const showData = await getSubtitleInfo(showId,paramLang,totalLink) 
+          
+          if(listData) {
+            res.json(showData)
+            return
+          }
+
+          const linkNum = showData.downloadLinks.length <= req.params.num ? showData.downloadLinks.length - 1 : req.params.num  
+          const url = showData.downloadLinks[linkNum].downloadLink.split('-')[1]
+          const options =  { 
+              method: 'GET',
+              url,
+              responseType: "arraybuffer"
+          };
+          const { data } = await axios(options);
+          const zip = new admZip(data)
+          const entries = zip.getEntries() 
+          let srtFileStream = null 
+          res.setHeader('Content-Type', 'text/plain; charset=utf-8')
+  
+          for (const entry of entries) {
+            if (entry.entryName.endsWith('.srt')) { 
+              const buffer = entry.getData() 
+              srtFileStream = Readable.from(buffer.toString('utf-8'))
+              break
+            }
+          } 
+          
+          if(srtFileStream) {
+              srtFileStream.pipe(res)
+          } else {
+              res.status(404).send('SRT file not found in the zip archive.')
+          }
+      } else {
+          res.status(404).send('No subtitle found in this language!')
+      }
+      
+  } catch (err) {
+      console.log(err)
+      res.status(500).send('Internal server error');
+  }
+}
 
 async function readMovieSubtitle(req,res) {
   const paramLang =  req.params.lang //req.query.lang.split('.')[0]
@@ -15,7 +72,7 @@ async function readMovieSubtitle(req,res) {
       if(paramLang) {
           const totalLink = req.params.totLink
           const movieName = req.params.movieName;
-          const movieId = await getMovieId(movieName,paramLang);
+          const movieId = await getId(movieName,paramLang);
           const movieData = await getSubtitleInfo(movieId,paramLang,totalLink) 
           const linkNum = movieData.downloadLinks.length <= req.params.num ? movieData.downloadLinks.length - 1 : req.params.num  
           const url = movieData.downloadLinks[linkNum].downloadLink.split('-')[1]
@@ -54,7 +111,8 @@ async function readMovieSubtitle(req,res) {
 }
 
 async function getMovie (req, res) { 
-    // @param url: api/tur/the-platform?totalLink=number
+    // @param url: api/search/tur/the-platform
+    // @param queries: totalLink=number
 
     let queryTotalLink = req.query.totalLink
     if(!queryTotalLink) {
@@ -63,7 +121,7 @@ async function getMovie (req, res) {
     try {
       const paramLang = req.params.lang
       const movieName = req.params.movieName;
-      const movieId = await getMovieId(movieName,paramLang);
+      const movieId = await getId(movieName,paramLang);
       if (!movieId) {
         throw new Error('Movie not found');
       }
@@ -75,31 +133,53 @@ async function getMovie (req, res) {
     }
 }
 
-async function getMovieId(movieName,lang,show) {
+async function getShow (req, res) { 
+  // @param url: api/search/lang*/the-platform[?totalLink=number]
+
+  let queryTotalLink = req.query.totalLink
+  if(!queryTotalLink) {
+      queryTotalLink = 3
+  } 
+  try {
+    const paramLang = req.params.lang
+    const showName = req.params.showName;
+    const showId = await getId(showName,paramLang);
+    if (!showId) {
+      throw new Error('Movie not found');
+    }
+    
+    const subtitleInfo = await getSubtitleInfo(showId,paramLang,queryTotalLink); 
+    res.json(subtitleInfo);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
+
+async function getId(mediaName,lang,show) {
     const searchUrl = `https://www.opensubtitles.org/en/search2/sublanguageid-${lang}/moviename-${encodeURIComponent(
-      movieName
+      mediaName
     )}`;
     try {
       const searchResponse = await axios.get(searchUrl);
       const $ = cheerio.load(searchResponse.data);
-      const movieLink = $('.bnone').attr('href'); 
-      if (!movieLink) {
+      const mediaLink = $('.bnone').attr('href'); 
+      if (!mediaLink) {
         return null;
       }
       
-      const movieId = movieLink.match(/\/idmovie-(\d+)/);
-      if (!movieId || movieId.length < 2) {
+      const mediaId = mediaLink.match(/\/idmovie-(\d+)/);
+      if (!mediaId || mediaId.length < 2) {
         return null;
       }
       
-      return movieId[1];
+      return mediaId[1];
     } catch (error) {
       throw new Error('Movie search failed');
     }
 }
   
-async function getSubtitleInfo(movieId,lang,totalLink) {
-    const movieUrl = `https://www.opensubtitles.org/en/search/sublanguageid-${lang}/idmovie-${movieId}`
+async function getSubtitleInfo(mediaId,lang,totalLink) {
+    const movieUrl = `https://www.opensubtitles.org/en/search/sublanguageid-${lang}/idmovie-${mediaId}`
     try {
       const movieResponse = await axios.get(movieUrl)
       const $ = cheerio.load(movieResponse.data) 
